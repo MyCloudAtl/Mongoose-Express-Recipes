7a91a7c4893a1923717603515ec3c6702c61307b
MyCloudAtl <megandmashburn@gmail.com>
2024-05-28 12:13:47 -0400

    building seeds

--- seed/recipe.js
+++ seed/recipe.js
@@ -1,1 +1,30 @@
 const Mongoose = require('Mongoose')
+const db = require('../db')
+const { Cuisine, Recipe } = require('../models')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const theGodfather = await Movie.find({ title: 'The Godfather' })
+   
+  
+    const recipes = [
+      {
+          firstName: `Brad`,
+          lastName: `Pitt`,
+          age: 58,
+          deceased: false,
+          // url: { type: String, required: true },
+          movie_id: fightClub[0]._id
+      },
+    ]
+  
+    await Recipe.insertMany(recipes)
+    console.log('Created recipes with cuisines!')
+  }
+  const run = async () => {
+    await main()
+    db.close()
+  }
+  
+  run()
