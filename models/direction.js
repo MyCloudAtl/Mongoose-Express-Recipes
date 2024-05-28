c30d2f4a5ea9efed453cde8e7ba5233ba548d034
GideonCove <gideoncove@gmail.com>
2024-05-28 12:15:53 -0400

    m

--- models/direction.js
+++ models/direction.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose')
+const { Schema } = require('mongoose')
+
+const Direction = new Schema (
+    {
+        directions: {type: String, required: true}
+    }
+)
--- models/recipe.js
+++ models/recipe.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose')
+const { Schema } = require('mongoose')
+
+const Recipe = new Schema (
+    {
+        cuisine: {type: Schema.Types.ObjectId, ref: 'cuisine_id'},
+        prepTime: {type: Number, required: true},
+        ingredients: {type: String, required: true},
+        directions: {type: Schema.Types.ObjectId, ref: 'direction_id'}
+    },
+    {timestamps: true}
+)
+
+module.exports = mongoose.model('recipes', Recipe)
 //cuisine parent of recipes which is parent of directions
